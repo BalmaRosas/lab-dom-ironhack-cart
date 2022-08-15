b48a062b2e06839ae2b134cda837d0a698068705
“Balma <balma.rosas@gmail.com>
2022-08-15 22:00:54 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,20 +1,52 @@
 // ITERATION 1
 
-function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+// function updateSubtotal(product) {
+//   console.log('Calculating subtotal, yey!');
+
 
   //... your code goes here
+//}
+
+function updateSubtotal(product) {
+  // Steps 1 & 2 el innerhtml te coge lo que le dices en el parentesis
+  const price = product.querySelector('.price span').innerHTML;
+  const quantity = product.querySelector('.quantity input').value;
+  // Step 3
+  const subtotal = Number(price * quantity);
+  // Step 4
+  const subtotalElement = product.querySelector('.subtotal span');
+  // Step 5
+  subtotalElement.innerHTML = subtotal;
+  return subtotal;
 }
 
-function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+// function calculateAll() {
+//   // code in the following two lines is added just for testing purposes.
+//   // it runs when only iteration 1 is completed. at later point, it can be removed.
+//   const singleProduct = document.querySelector('.product');
+//   updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  
+  function calculateAll(arrayOfProducts) {
+    // let acumulable = 0
+    // for (let i = 0; i < arrayOfProducts.length ; i++) {
+    //   const prices = updateSubtotal(arrayOfProducts[i])
+    //   acumulable += prices
+    // }
+    // return acumulable
+
+    var pricesArrow = 0 ; 
+    pricesArrow += updateSubtotal((element) => {
+    return updateSubtotal(arrayOfProducts[element])
+    })
+    
+  }
+  
+
+
 
   // ITERATION 3
   //... your code goes here
